fd68589702307cc1f0c9e99a05b663409b6d5f34
Mena <Mena@Smartit.Local>
2026-07-22 16:29:16 +0300

    Register Gate Glass import service

--- src/glasses/glasses.module.ts
+++ src/glasses/glasses.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
+
+import { PrismaService } from '../prisma/prisma.service';
+
 import { GlassesController } from './glasses.controller';
+import { GlassesImportService } from './glasses-import.service';
 import { GlassesService } from './glasses.service';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [GlassesController],
-  providers: [GlassesService],
-  exports: [GlassesService],
+  imports: [],
+
+  controllers: [
+    GlassesController,
+  ],
+
+  providers: [
+    PrismaService,
+    GlassesService,
+    GlassesImportService,
+  ],
+
+  exports: [
+    GlassesService,
+    GlassesImportService,
+  ],
 })
 export class GlassesModule {}
